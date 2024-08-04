14a6e7e14248aaaf49d5a0d84d024b58660907ee
piotr <aranruth94@gmail.com>
2024-08-04 22:20:42 +0200

    Add email sender service

---
+++ book-network/src/main/java/com/piotr/book_network/email/EmailTemplateName.java
@@ -0,0 +1,13 @@
+package com.piotr.book_network.email;
+
+import lombok.Getter;
+
+@Getter
+public enum EmailTemplateName {
+    ACTIVATE_ACCOUNT("activate-account");
+    private final String name;
+
+    EmailTemplateName(String name) {
+        this.name = name;
+    }
+}
