e9160d3fc659492d90419f0dc770bda8ef43fe6d
piotr <aranruth94@gmail.com>
2024-08-15 15:47:44 +0200

    Moved project to no standalone angular version

--- book-network-ui/src/app/app.module.ts
+++ book-network-ui/src/app/app.module.ts
@@ -1,22 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { HttpClientModule } from '@angular/common/http';
-import { AppComponent } from './app.component';
-import { HttpClient } from '@angular/common/http';
+
 import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { LoginComponent } from './pages/login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule,
+    HttpClientModule
   ],
   providers: [
-    HttpClient
-  ],
+    HttpClient],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
